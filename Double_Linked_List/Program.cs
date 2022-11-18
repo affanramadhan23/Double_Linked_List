cedfb48760e855243eb60dada96f0c20fa320ae4
affanramadhan23 <affannabilramadhan@gmail.com>
2022-11-18 16:50:35 +0700

    add method class node

--- Double_Linked_List/Program.cs
+++ Double_Linked_List/Program.cs
@@ -6,6 +6,19 @@ using System.Threading.Tasks;
 
 namespace Double_Linked_List
 {
+    class Node
+    {
+        //* Node class represent the node of doubly linked list
+        //* It consist of the information part and links to
+        //* Its succeding and preceeding
+        //* i terms of next and previous 
+        public int noMhs;
+        public string name;
+        //point to the succeding node
+        public Node next;
+        //point to the preceeding node
+        public Node prev;
+    }
     internal class Program
     {
         static void Main(string[] args)
