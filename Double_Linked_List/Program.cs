1539adcdff0716a5ba4f91a6384110ea27a47afb
affanramadhan23 <affannabilramadhan@gmail.com>
2022-11-18 20:07:26 +0700

    add function class program and method while, try, switch

--- Double_Linked_List/Program.cs
+++ Double_Linked_List/Program.cs
@@ -163,6 +163,33 @@ namespace Double_Linked_List
             }
         }
     }
+
+    class program
+    {
+        static void Main(string[] args)
+        {
+            DoubleLinkedList obj = new DoubleLinkedList();
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("/nMenu");
+                    Console.WriteLine("1. Add a record to the list");
+                    Console.WriteLine("2. Delete a record from the list");
+                    Console.WriteLine("3. View all records in the ascending order of roll numbers");
+                    Console.WriteLine("4. View all recors in the descending order of roll numbers");
+                    Console.WriteLine("5. Search for a record in the list");
+                    Console.WriteLine("6. Exit\n");
+                    Console.Write("Enter your choice (1-6): ");
+                    char ch = Convert.ToChar(Console.ReadLine());
+                    switch (ch)
+                    {
+
+                    }
+                }
+            }
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
